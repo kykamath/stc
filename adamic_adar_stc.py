ac587cbbdaf758386c5318cafcb3587364b3e560
Krishna Kamath <kkamath@twitter.com>
2014-12-09 08:58:22 -0800

    Modified files

---
+++ adamic_adar_stc.py
@@ -0,0 +1,93 @@
+from networkx.classes.function import common_neighbors
+from collections import defaultdict
+import math
+import networkx as nx
+
+def loadGraph(fileName):
+  def addLineToGraph(graph, line):
+    data = line.strip().split()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+    graph.add_edge(data[0], data[1])
+  graph = nx.Graph()
+  map(lambda l: addLineToGraph(graph, l), open(fileName)) 
+  return graph
+
+def getEdgeId(u, v): 
+  edge = sorted((u, v))
+  return "%s\t%s"%(edge[0], edge[1])
+
+def getEdge(edgeId): return map(lambda i: i, edgeId.split('\t'))
+
+def getAdamicScore(graph, u, v):
+  print u, v
+  def getLog2(f):
+    if f == 0 or f == 1: 
+      print "getLog2 called with f=%s. This is not valid. Stopping the program."%f
+      exit
+    return math.log(f, 2)
+  neighbors = list(common_neighbors(graph, u, v))
+  if neighbors:
+    total_score = sum(map(lambda n: 1/getLog2(len(graph.neighbors(n))), neighbors))
+    return total_score/len(neighbors)
+  else: 
+    print "Number of common neighbors between u and v is 0. This is not valid. Stopping the program."
+    exit
+
+def getSecondDegreeNodes(graph, u): 
+  return (v2 
+          for v1 in graph.neighbors(u) 
+           for v2 in graph.neighbors(v1) if u != v2)
+
+def fillAdamicScore(graph, node, mf_edge_to_score):
+  for v in getSecondDegreeNodes(graph, node):
+    edge_id = getEdgeId(node, v)
+    if edge_id not in mf_edge_to_score: mf_edge_to_score[edge_id] = getAdamicScore(graph, node, v)    
+
+def printTopEdgesPerNode(mf_edge_to_score):
+  mf_node_to_neighbor_w_score = defaultdict(dict)
+  for (id, score) in mf_edge_to_score.iteritems():
+    (u, v) = getEdge(id)
+    mf_node_to_neighbor_w_score[u][v] = score
+  for k, v in mf_node_to_neighbor_w_score.iteritems():
+    print k, ','.join('%s:%s'%(t[0], t[1]) for t in sorted(v.items(), key=lambda t: t[1]))
+    
+def printTopFollowEdgesPerNode(graph, mf_edge_to_score):
+  mf_node_to_neighbor_w_score = defaultdict(dict)
+  for (id, score) in mf_edge_to_score.iteritems():
+    (u, v) = getEdge(id)
+    mf_node_to_neighbor_w_score[u][v] = score
+  for k, v in mf_node_to_neighbor_w_score.iteritems():
+    neighbors = graph.neighbors(k)
+    sortedItems = sorted(v.items(), key=lambda t: t[1])
+    sortedItems = filter(lambda t: t[0] in neighbors, sortedItems)
+    print k, ','.join('%s:%s'%(t[0], t[1]) for t in sortedItems)
+    
+def printStrongTies(graph, userNode):
+  mf_edge_to_score = {}
+  mf_node_to_score = {}
+  print fillAdamicScore(graph, userNode, mf_edge_to_score)
+  for edge in mf_edge_to_score:
+    (u, v) = getEdge(edge)
+    node = None
+    if u != userNode: node = u
+    else: node = v
+    mf_node_to_score[node] = mf_edge_to_score[edge]
+  neighbors = graph.neighbors(userNode)
+  sortedItems = sorted(mf_node_to_score.items(), key=lambda t: t[1])
+  sortedItems = filter(lambda t: t[0] in neighbors, sortedItems)
+  print sortedItems
+
+def getSTCProbabilities(graph):
+  mf_edge_to_score = {}
+  num_nodes = graph.number_of_nodes()
+  for (i, v) in enumerate(graph.nodes()): 
+#     print 'Processing %s of %s'%(i, num_nodes)
+    fillAdamicScore(graph, v, mf_edge_to_score)
+#   printTopEdgesPerNode(mf_edge_to_score)
+  printTopFollowEdgesPerNode(graph, mf_edge_to_score)
+ 
+if __name__ == '__main__':
+#   getSTCProbabilities(loadGraph('data/demo_graph'))  
+#   getSTCProbabilities(loadGraph('data/edges_5867')) 
+  printStrongTies(loadGraph('data/edges_5867'), '18929196') 
+  
+  
--- vertex_cover.py
+++ vertex_cover.py
@@ -1,13 +1,28 @@
+from itertools import groupby
 from networkx.algorithms.approximation.vertex_cover import min_weighted_vertex_cover
 import networkx as nx
 import matplotlib.pyplot as plt
 import sys
+import random
+from scipy.stats.stats import pearsonr
+import numpy as np
+from collections import defaultdict
 
 def loadGraph(fileName):
   
   def addLineToGraph(graph, line):
     data = line.strip().split()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
     graph.add_edge(data[0], data[1])
+  
+  graph = nx.Graph()
+  map(lambda l: addLineToGraph(graph, l), open(fileName)) 
+  return graph
+
+def loadGraphWithWeight(fileName):
+  
+  def addLineToGraph(graph, line):
+    data = line.strip().split()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+    graph.add_edge(data[0], data[1], {'w': float(data[2])})
   
   graph = nx.Graph()
   map(lambda l: addLineToGraph(graph, l), open(fileName)) 
@@ -23,13 +38,12 @@ def writeVertexCover(vertexCover, fileName):
   f = open(fileName+'_vertex_cover', 'w')
   for v in vertexCover: f.write('%s\n'%v)
   f.close()
-  
+
+def getEdgeId(edge):
+  t = sorted(edge)
+  return '%s_%s'%(t[0], t[1])  
 
 def getGraphDual(graph):
-  
-  def getEdgeId(edge):
-    t = sorted(edge)
-    return '%s_%s'%(t[0], t[1])
   
   def addEdgeToDual(graph, dualGraph, edgeIt):
     it, edge = edgeIt
@@ -56,20 +70,112 @@ def getVertexCoverGreedy(graph):
 
   vertexCoverSet = set()
   nextVertexToRemove = maxDegreeNode(graph)
+  iteration = 1
   while nextVertexToRemove != None:
+    print 'Generating vertex cover. Iteration: ', iteration
+    iteration = iteration + 1
     vertexCoverSet.add(nextVertexToRemove)
     graph.remove_node(nextVertexToRemove)
     nextVertexToRemove = maxDegreeNode(graph)
   return vertexCoverSet
 
+#
+# python vertex_cover.py data/edges_5867_2
+#
+# 1.3319152651
+# 1.3501473505
+# (0.018432481710418454, 0.57022048147957038)
+
+def calculateDifferenceInRealGraphWeights(fileName):
+  graph = loadGraphWithWeight(fileName)
+  
+  dualGraph = getGraphDual(graph)
+  writeDualGraph(dualGraph, inputGraphFile)
+  
+  loadGraph()
+  
+  vertexCover = min_weighted_vertex_cover(dualGraph)
+  writeVertexCover(vertexCover, inputGraphFile)
+  
+  edgeIdToWeightMap = map(lambda e: (getEdgeId(e[:2]), e[2]['w']), graph.edges_iter(data=True))
+  
+  weakEdges = []
+  strongEdges = []
+  for e in graph.edges_iter(data=True):
+    edgeId = getEdgeId(e[:2])
+    weight = e[2]['w']
+    if edgeId in vertexCover: weakEdges.append(weight)
+    else: strongEdges.append(weight)
+ 
+  numElements = -1
+  if len(weakEdges) > len(strongEdges): numElements = len(strongEdges)
+  else: numElements = len(weakEdges)
+ 
+  print np.mean(weakEdges)
+  print np.mean(strongEdges)
+  print pearsonr(random.sample(weakEdges, numElements), random.sample(strongEdges, numElements))
+  
+  
+
+def printLabeledEdges(fileName):
+  graph = loadGraphWithWeight(fileName)
+  
+  dualGraph = loadGraph(fileName+'_dual')
+#   writeDualGraph(dualGraph, inputGraphFile)
+  
+  vertexCover = min_weighted_vertex_cover(dualGraph)
+  writeVertexCover(vertexCover, inputGraphFile)
+  
+  edgeIdToWeightMap = map(lambda e: (getEdgeId(e[:2]), e[2]['w']), graph.edges_iter(data=True))
+  
+  weakEdges = []
+  strongEdges = []
+  for e in graph.edges_iter(data=True):
+    edgeId = getEdgeId(e[:2])
+    weight = e[2]['w']
+    if edgeId in vertexCover: print edgeId.replace('_', ' '), '0' 
+    else: print edgeId.replace('_', ' '), '1'
+ 
+def printDegreeWithStrongTies(fileName):
+  graph = loadGraphWithWeight(fileName)
+  labeledGraph = loadGraphWithWeight(fileName+'_labeled')
+  degreeWithStrongTiesCount = defaultdict(int)
+  for e in labeledGraph.edges(data=True):
+    degreeWithStrongTiesCount[e[0]]+=e[2]['w']
+    degreeWithStrongTiesCount[e[1]]+=e[2]['w']
+  
+  for k,v in graph.degree().iteritems():
+    print k, v, degreeWithStrongTiesCount.get(k, 0.0)
+ 
+def degreeDistribution(fileName):
+  graph = loadGraphWithWeight(fileName)
+  
+#   graph = nx.Graph()
+  degrees = sorted(graph.degree().values())
+  degreeDist = map(lambda (k, v): (k, len(list(v))), groupby(degrees, lambda k: k))
+  x, y = zip(*sorted(degreeDist, key = lambda t: t[0]))
+  
+  plt.loglog(x, y, 'o', c = 'k')
+  plt.xlabel('Num. of neighbors')
+  plt.ylabel('Num. of nodes')
+  plt.show()
+
 
 if __name__ == '__main__':
   inputGraphFile = sys.argv[1]
-  graph = loadGraph(inputGraphFile)
-  dualGraph = getGraphDual(graph)
-  writeDualGraph(dualGraph, inputGraphFile)
-  writeVertexCover(getVertexCoverGreedy(dualGraph), inputGraphFile)
+#   graph = loadGraph(inputGraphFile)
+#   dualGraph = getGraphDual(graph)
+#   writeDualGraph(dualGraph, inputGraphFile)
+#   writeVertexCover(getVertexCoverGreedy(dualGraph), inputGraphFile)
+
+#   writeVertexCover(getVertexCoverGreedy1(loadGraph(inputGraphFile)), inputGraphFile)
+
+#   writeVertexCover(min_weighted_vertex_cover(loadGraph(inputGraphFile)), inputGraphFile)
+
+#   calculateDifferenceInRealGraphWeights(inputGraphFile)
+#   printLabeledEdges(inputGraphFile)
+#   degreeDistribution(inputGraphFile)
+  printDegreeWithStrongTies(inputGraphFile)
   
-# print min_weighted_vertex_cover(getGraphDual(graph))
 
 
