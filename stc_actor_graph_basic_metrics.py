a505246786ad40a001a18f26d3588d12170707e1
Krishna Kamath <kkamath@twitter.com>
2015-06-08 07:47:55 -0700

    Modified files

---
+++ stc_actor_graph_basic_metrics.py
@@ -0,0 +1,34 @@
+import networkx as nx
+import random
+
+# cut -f1 actors_2500_0.65_6955_small | awk 'NR%2'| uniq
+
+def load_graph(graph_file):
+  graph = nx.Graph()
+  for (u, v, w) in map(lambda l: map(int, l.strip().split()), open(graph_file)):
+    graph.add_edge(u, v, {'orig_weight': w})
+  return graph
+
+def outputFormat(u, v, score): return "\t".join(map(str, [u, v, score])) + "\n"
+
+def mutualFollow(graph, output_file_name):
+  outputFile = open(output_file_name + "_mutualFollow", 'w')
+  metric_name = "mutualFollow"
+  for (u, v) in graph.edges():
+    line = outputFormat(u, v, len(set(graph.neighbors(u)).intersection(graph.neighbors(v))))
+    outputFile.write(line)
+  outputFile.close()
+
+def adamic_adar(u, v, graph): 
+  for e in nx.adamic_adar_index(graph): print e
+
+if __name__ == '__main__':
+  graph_file = 'data/actors_2500.graph'
+  num_edges = 700
+  graph = load_graph(graph_file)
+  
+#   5619
+#   67154
+  
+  mutualFollow(graph, "data/actors_2500_%s"%num_edges)
+#     print mutualFollow(u, v, graph)
